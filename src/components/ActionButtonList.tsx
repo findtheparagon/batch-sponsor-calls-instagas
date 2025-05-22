55a99a795378666357436b289edb6b2d863d546c
Leandro Yalet <leandro.yalet@gmail.com>
2025-05-21 23:02:27 -0300

    chore: branding + inputs

--- src/components/ActionButtonList.tsx
+++ src/components/ActionButtonList.tsx
@@ -1,67 +1,44 @@
-import { useEffect } from 'react';
-import { useDisconnect, useAppKit, useAppKitAccount } from '@reown/appkit/react';
-import { Hex, parseGwei, toHex, type Address, Capabilities, zeroAddress, parseUnits, encodeFunctionData } from 'viem';
 import {
-  useChainId,
-  useSendTransaction,
-  useSendCalls,
+  useAppKit,
+  useAppKitAccount,
+  useDisconnect
+} from "@reown/appkit/react";
+import { ChangeEvent, useEffect, useState } from "react";
+import {
+  Capabilities,
+  encodeFunctionData,
+  parseGwei,
+  parseUnits,
+  toHex,
+  type Address
+} from "viem";
+import {
   useCallsStatus,
-  useCapabilities
-} from 'wagmi';
+  useCapabilities,
+  useChainId,
+  useSendCalls,
+  useSendTransaction
+} from "wagmi";
 
-import { erc20Abi, multiwrapAbi } from '../abi';
+import { erc20Abi, multiwrapAbi } from "../abi";
 
-const erc20Address = '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238';
-const multiwrapAddress = '0x0Ec8C4C80E4965381999C281C5a7173a9cd30cfD';
-const amount = parseUnits('5', 6); // 5 usdc
-const recipient = '0x27764f3d7075dc999e33D5F6440C779C5fe65eD5';
-const uri = '';
-
-const data1 = encodeFunctionData({
-    abi: erc20Abi,
-    functionName: 'approve',
-    args: [ multiwrapAddress, amount ]
-});
-
-// Test transaction
-const tx1 = {
-  to: erc20Address,
-  value: parseGwei('0'),
-  data: data1
-};
-
-const tokensToWrap = [{
-    assetContract: erc20Address,
-    tokenType: 0,
-    tokenId: 5,
-    totalAmount: amount
-}];
-
-const data2 = encodeFunctionData({
-  abi: multiwrapAbi,
-  functionName: 'wrap',
-  args: [tokensToWrap, uri, recipient]
-});
-
-const tx2 = {
-  to: multiwrapAddress,
-  value: parseGwei('0'),
-  data: data2
-};
+const erc20Address = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
+const multiwrapAddress = "0x0Ec8C4C80E4965381999C281C5a7173a9cd30cfD";
+const amount = parseUnits("5", 6); // 5 usdc
 
 interface ActionButtonListProps {
   sendHash: (hash: `0x${string}`) => void;
-  sendCapabilities: (capabilities: Capabilities) => void
-  sendStatus: (status: string | undefined) => void
-  sendError: (error: string) => void
+  sendCapabilities: (capabilities: Capabilities) => void;
+  sendStatus: (status: string | undefined) => void;
+  sendError: (error: string) => void;
 }
 
 const chainIdToNetwork = {
-  1: 'ethereum',
-  84532: 'base-sepolia',
-  11155111: 'sepolia',
-  10: 'optimism',
-  31337: 'anvil',
+  1: "ethereum",
+  84532: "base-sepolia",
+  11155111: "sepolia",
+  10: "optimism",
+  31337: "anvil"
 };
 
 const chainIdToSponsorshipPolicyId = {
@@ -69,10 +46,10 @@ const chainIdToSponsorshipPolicyId = {
   84532: import.meta.env.VITE_BASE_SEPOLIA_SPONSORSHIP_POLICY_ID,
   11155111: import.meta.env.VITE_SEPOLIA_SPONSORSHIP_POLICY_ID,
   10: import.meta.env.VITE_OPTIMISM_SPONSORSHIP_POLICY_ID,
-  31337: import.meta.env.VITE_ANVIL_SPONSORSHIP_POLICY_ID,
+  31337: import.meta.env.VITE_ANVIL_SPONSORSHIP_POLICY_ID
 };
 
-const candideApiKey = import.meta.env.VITE_CANDIDE_APY_KEY
+const candideApiKey = import.meta.env.VITE_CANDIDE_APY_KEY;
 const candidePaymasterVersion = "v3";
 
 export const ActionButtonList = ({
@@ -81,6 +58,26 @@ export const ActionButtonList = ({
   sendStatus,
   sendError
 }: ActionButtonListProps) => {
+  const [formData, setFormData] = useState({
+    recipient: "",
+    uri: ""
+  });
+
+  const [validRecipient, setValidRecipient] = useState(true);
+  const [validUri, setValidUri] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+
+  const ethAddressRegex = /^0x[a-fA-F0-9]{40}$/;
+  const uriRegex = /^(https?:\/\/|ipfs:\/\/)[^\s]+$/;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
   const chainId = useChainId() as keyof typeof chainIdToNetwork;
 
   const sponsorshipPolicyId = chainIdToSponsorshipPolicyId[chainId];
@@ -91,15 +88,23 @@ export const ActionButtonList = ({
   const { address, isConnected } = useAppKitAccount(); // AppKit hook to get the address and check if the user is connected
 
   const { data: capabilities } = useCapabilities({
-    account: address as Address,
+    account: address as Address
   }); // Wagmi hook to check wallet capabilities (for sponsorship)
   const { sendCalls, data: id } = useSendCalls(); // Wagmi hook to send sponsored and batch transactions
   const { data: hash, sendTransaction } = useSendTransaction(); // Wagmi hook to send standard transaction
 
   useEffect(() => {
+    const isRecipientValid = ethAddressRegex.test(formData.recipient);
+    const isUriValid = uriRegex.test(formData.uri);
+    setValidRecipient(isRecipientValid || formData.recipient === "");
+    setValidUri(isUriValid || formData.uri === "");
+    setIsValid(isRecipientValid && isUriValid);
+  }, [formData]);
+
+  useEffect(() => {
     if (hash) {
       sendHash(hash);
-      console.log("Hash: ", hash)
+      console.log("Hash: ", hash);
     }
   }, [hash]);
 
@@ -111,9 +116,42 @@ export const ActionButtonList = ({
     }
   }, [capabilities, address, chainId]);
 
-
   // Function to send transactions
   const handleSendTx = () => {
+    const data1 = encodeFunctionData({
+      abi: erc20Abi,
+      functionName: "approve",
+      args: [multiwrapAddress, amount]
+    });
+
+    // Test transaction
+    const tx1 = {
+      to: erc20Address,
+      value: parseGwei("0"),
+      data: data1
+    };
+
+    const tokensToWrap = [
+      {
+        assetContract: erc20Address,
+        tokenType: 0,
+        tokenId: 5,
+        totalAmount: amount
+      }
+    ];
+
+    const data2 = encodeFunctionData({
+      abi: multiwrapAbi,
+      functionName: "wrap",
+      args: [tokensToWrap, formData.uri, formData.recipient]
+    });
+
+    const tx2 = {
+      to: multiwrapAddress,
+      value: parseGwei("0"),
+      data: data2
+    };
+
     // if smart capabilities are supported, send a sponsored batched transaction
     try {
       if (!capabilities) {
@@ -124,8 +162,10 @@ export const ActionButtonList = ({
       }
 
       const atomicStatus = capabilities[chainId].atomic?.status;
-      const isAtomicSupported = atomicStatus === "supported" || atomicStatus === "ready";
-      const isPaymasterSupported = capabilities[chainId].paymasterService?.supported;
+      const isAtomicSupported =
+        atomicStatus === "supported" || atomicStatus === "ready";
+      const isPaymasterSupported =
+        capabilities[chainId].paymasterService?.supported;
 
       if (isAtomicSupported) {
         if (isPaymasterSupported) {
@@ -138,15 +178,15 @@ export const ActionButtonList = ({
                   url: paymasterUrl,
                   optional: true,
                   context: {
-                    sponsorshipPolicyId,
+                    sponsorshipPolicyId
                   }
                 }
               }
-            },
+            }
           });
         } else {
           sendCalls({
-            calls: [tx1, tx2],
+            calls: [tx1, tx2]
           });
         }
       } else {
@@ -155,19 +195,19 @@ export const ActionButtonList = ({
         sendTransaction(tx2);
       }
     } catch (err) {
-      sendError(`Error sending transaction:'${err}`)
-      console.log('Error sending transaction:', err);
+      sendError(`Error sending transaction:'${err}`);
+      console.log("Error sending transaction:", err);
     }
   };
 
   // get status of sendCalls
   const { data: callStatusData, refetch: refetchCallStatus } = useCallsStatus({
-    id: id?.id || '',
+    id: id?.id || "",
     query: {
       enabled: !!id,
-      refetchInterval: (data) =>
-        data.state.data?.status === "success" ? false : 1000,
-    },
+      refetchInterval: data =>
+        data.state.data?.status === "success" ? false : 1000
+    }
   });
 
   useEffect(() => {
@@ -188,17 +228,61 @@ export const ActionButtonList = ({
     try {
       await disconnect();
     } catch (error) {
-      sendError(`Error sending transaction:'${error}`)
+      sendError(`Error sending transaction:'${error}`);
       console.error("Failed to disconnect:", error);
     }
   };
 
   return (
     isConnected && (
-      <div>
-        <button onClick={() => open()}>Open</button>
-        <button onClick={handleDisconnect}>Disconnect</button>
-        <button onClick={handleSendTx}>Send tx</button>
+      <div className="w-full">
+        <div className="flex gap-x-2 my-4 justify-center items-center">
+          <button className="btn" onClick={() => open()}>
+            Open
+          </button>
+          <button className="btn" onClick={handleDisconnect}>
+            Disconnect
+          </button>
+        </div>
+        <div className="w-full">
+          <div className="flex flex-col items-center gap-4 p-6">
+            <div className="w-full max-w-sm">
+              <label className="block text-sm font-medium text-stone-700">
+                Recipient
+              </label>
+              <input
+                type="text"
+                name="recipient"
+                placeholder="0x..."
+                value={formData.recipient}
+                onChange={handleChange}
+                className={`input ${validRecipient ? "" : "invalid"}`}
+              />
+            </div>
+
+            <div className="w-full max-w-sm">
+              <label className="block text-sm font-medium text-stone-700">
+                URI
+              </label>
+              <input
+                type="text"
+                name="uri"
+                placeholder="ipfs://..."
+                value={formData.uri}
+                onChange={handleChange}
+                className={`input ${validUri ? "" : "invalid"}`}
+              />
+            </div>
+
+            <button
+              className={`btn ${!isValid ? "btn-disabled" : ""}`}
+              onClick={handleSendTx}
+              disabled={!isValid}
+            >
+              Send tx
+            </button>
+          </div>
+        </div>
       </div>
     )
   );
--- src/components/InfoList.tsx
+++ src/components/InfoList.tsx
@@ -1,75 +1,105 @@
-import { useAppKitState } from '@reown/appkit/react'
-import { useChainId, useWaitForTransactionReceipt } from 'wagmi'
-import type { Capabilities } from 'viem';
+import { useAppKitState } from "@reown/appkit/react";
+import { useChainId, useWaitForTransactionReceipt } from "wagmi";
+import type { Capabilities } from "viem";
 
 interface InfoListProps {
-    hash: `0x${string}` | undefined;
-    capabilities: Capabilities | undefined;
-    status: string | undefined;
-    error: string;
+  hash: `0x${string}` | undefined;
+  capabilities: Capabilities | undefined;
+  status: string | undefined;
+  error: string;
 }
-  
-export const InfoList = ({ hash, capabilities, status, error }: InfoListProps) => {
-    const state = useAppKitState(); // AppKit hook to get the state
-    const chainId = useChainId();
 
-    const { data: receipt } = useWaitForTransactionReceipt({
-        hash, confirmations: 2,  // Wait for at least 2 confirmation
-        timeout: 300000,    // Timeout in milliseconds (5 minutes)
-        pollingInterval: 1000,
-    })
+export const InfoList = ({
+  hash,
+  capabilities,
+  status,
+  error
+}: InfoListProps) => {
+  const state = useAppKitState(); // AppKit hook to get the state
+  const chainId = useChainId();
 
-    if (capabilities && capabilities[chainId]) {
-        console.log(capabilities[chainId].atomic);
-    }
+  const { data: receipt } = useWaitForTransactionReceipt({
+    hash,
+    confirmations: 2, // Wait for at least 2 confirmation
+    timeout: 300000, // Timeout in milliseconds (5 minutes)
+    pollingInterval: 1000
+  });
 
-    return (
-        <>
-            {(status || hash) && (
-                <section>
-                    <h2>Transaction Hash</h2>
-                    <pre>
-                        Hash: {hash}<br />
-                        Status: {status || receipt?.status.toString()}<br />
-                    </pre>
-                </section>
-            )}
-            {error && (
-                <section>
-                    <h2>Error</h2>
-                    <pre>{error}</pre>
-                </section>
-            )}
+  if (capabilities && capabilities[chainId]) {
+    console.log(capabilities[chainId].atomic);
+  }
 
-            <section>
-                <h2>Smart Capabilities</h2>
-                <pre>
-                    If the wallet supports smart capabilities, the dApp will send sponsored smart calls.
-                    <br />
-                    If the wallet does not support smart capabilities, the dApp will fallback to sending standard tx calls.
-                    <br />
-                    <br />
-                    <b>Capabilities for chainId {chainId}:</b>
-                    <br />
-                    {capabilities && capabilities[chainId] ? (
-                        <ul>
-                            <li>Atmoic Batching: {capabilities[chainId].atomic?.status ? capabilities[chainId].atomic.status : 'No'}</li>
-                            <li>Paymaster Service Supported: {capabilities[chainId].paymasterService?.supported ? 'Yes' : 'No'}</li>
-                            <li>Auxiliary Funds Supported: {capabilities[chainId].auxiliaryFunds?.supported === 'Yes' || 'No'}</li>
-                        </ul>
-                    ) : "undefined"}
-                </pre>
-            </section>
+  return (
+    <>
+      {(status || hash) && (
+        <section>
+          <h2>Transaction Hash</h2>
+          <pre>
+            Hash: {hash}
+            <br />
+            Status: {status || receipt?.status.toString()}
+            <br />
+          </pre>
+        </section>
+      )}
+      {error && (
+        <section>
+          <h2>Error</h2>
+          <pre>{error}</pre>
+        </section>
+      )}
 
-            <section>
-                <h2>State</h2>
-                <pre>
-                    activeChain: {state.activeChain}<br />
-                    loading: {state.loading.toString()}<br />
-                    open: {state.open.toString()}<br />
-                    selectedNetworkId: {state.selectedNetworkId?.toString()}<br />
-                </pre>
-            </section>
-        </>
-    )
-}
+      <section>
+        <h2>Smart Capabilities</h2>
+        <div>
+          If the wallet supports smart capabilities, the dApp will send
+          sponsored smart calls.
+          <br />
+          If the wallet does not support smart capabilities, the dApp will
+          fallback to sending standard tx calls.
+          <br />
+          <br />
+          <b>Capabilities for chainId {chainId}:</b>
+          <br />
+          {capabilities && capabilities[chainId] ? (
+            <ul>
+              <li>
+                Atmoic Batching:{" "}
+                {capabilities[chainId].atomic?.status
+                  ? capabilities[chainId].atomic.status
+                  : "No"}
+              </li>
+              <li>
+                Paymaster Service Supported:{" "}
+                {capabilities[chainId].paymasterService?.supported
+                  ? "Yes"
+                  : "No"}
+              </li>
+              <li>
+                Auxiliary Funds Supported:{" "}
+                {capabilities[chainId].auxiliaryFunds?.supported === "Yes" ||
+                  "No"}
+              </li>
+            </ul>
+          ) : (
+            "undefined"
+          )}
+        </div>
+      </section>
+
+      <section>
+        <h2>State</h2>
+        <div>
+          activeChain: {state.activeChain}
+          <br />
+          loading: {state.loading.toString()}
+          <br />
+          open: {state.open.toString()}
+          <br />
+          selectedNetworkId: {state.selectedNetworkId?.toString()}
+          <br />
+        </div>
+      </section>
+    </>
+  );
+};
